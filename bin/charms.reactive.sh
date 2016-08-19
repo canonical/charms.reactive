1c1488560c6e9baca8820e41ea9684da3f70d87d
Cory Johns <johnsca@gmail.com>
2016-08-19 17:19:00 -0400

    Suppress xtrace for internal functions in charms.reactive.sh (fixes #80)

--- bin/charms.reactive.sh
+++ bin/charms.reactive.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+
+{
+    shopt -s expand_aliases
+    alias _suppress_xtrace='{ local old_opts="$-"; set $X_OPT; } 2> /dev/null'
+    alias _restore_xtrace='_restore_opt $old_opts x'
+    function _restore_opt() {
+        if [[ "$1" == *$2* ]]; then
+            set -$2
+        else
+            set +$2
+        fi
+    }
+    if [[ "$CHARMS_REACTIVE_TRACE" != 'true' ]]; then
+        X_OPT="+x"
+    else
+        X_OPT="-x"
+    fi
+    import_opts="$-"
+    set +x
+} 2> /dev/null  # suppress xtrace during import
+
 export PATH=$PATH:$CHARM_DIR/bin
 export PYTHONPATH=${PYTHONPATH-}:$CHARM_DIR/lib
 
@@ -41,8 +62,6 @@ if [[ "$0" == "$BASH_SOURCE" ]]; then
     exit 0
 fi
 
-shopt -s expand_aliases
-
 REACTIVE_ACTION="${1-'--test'}"
 REACTIVE_ARGS="${2-}"
 
@@ -52,13 +71,21 @@ declare -A REACTIVE_TESTS
 function _get_decorated() {
     filename=${BASH_SOURCE[2]}
     lineno=$((BASH_LINENO[1]+1))
+    last_decorator=yes
     while sed "${lineno}q;d" $filename | grep -qE "^\s*@"; do
+        last_decorator=no
         ((lineno++))
     done
     func=$(sed "${lineno}q;d" $filename | sed "s/\(function\)\? *\(.*\) *() *{/\2/")
+    if [[ "$old_opts" == *x* && $last_decorator == yes ]]; then
+        # pseudo-xtrace function definition when xtrace is active, since
+        # the @decorator line will be logged anyway (can't suppress it)
+        >&2 echo "+ function $func() { ... }"
+    fi
 }
 
 function @decorator() {
+    _suppress_xtrace
     _get_decorated
     handler_id="$filename:$lineno:$func"
 
@@ -75,25 +102,36 @@ function @decorator() {
     else
         REACTIVE_TESTS[$func]="${REACTIVE_TESTS[$func]} '$test_name' '$test_args'"
     fi
+    _restore_xtrace
 }
 
 function reactive_handler_main() {
+    _suppress_xtrace
     if [[ "$REACTIVE_ACTION" == "--test" ]]; then
+        >&2 echo "Running reactive_handler_main for $(basename $0) (test)"
         to_invoke=$(charms.reactive test "${REACTIVE_TESTS[@]}")
         if [[ -n "$to_invoke" ]]; then
             echo $to_invoke
+            >&2 echo "Will invoke: $to_invoke"
+            >&2 echo "End reactive_handler_main (test)"
             exit 0
         else
+            >&2 echo "Nothing to invoke"
+            >&2 echo "End reactive_handler_main (test)"
             exit 1
         fi
     elif [[ "$REACTIVE_ACTION" == "--invoke" ]]; then
+        >&2 echo "Running reactive_handler_main for $(basename $0) (invoke)"
         invoked=()
         for handler in ${REACTIVE_ARGS//,/ }; do
-            eval "$handler"
+            >&2 echo "Invoking bash reactive handler: $handler"
+            eval "_restore_xtrace; $handler; _suppress_xtrace"
             invoked+=("${REACTIVE_HANDLERS[$handler]}")
         done
         charms.reactive mark_invoked "${invoked[@]}"
+        >&2 echo "End reactive_handler_main (invoke)"
     fi
+    _restore_xtrace
 }
 
 alias @hook='@decorator hook'
@@ -112,15 +150,18 @@ alias all_states='charms.reactive all_states'
 alias any_states='charms.reactive any_states'
 
 function state_relation_call() {
+    _suppress_xtrace
     # Call a method on the relation implementation associated with the
     # given active state.
     state=$1
     method=$2
     shift; shift
     charms.reactive relation_call $(charms.reactive relation_from_state $state) $method "$@"
+    _restore_xtrace
 }
 
 function name_relation_get() {
+    _suppress_xtrace
     # Helper function around relation-get that accepts a relation name in place
     # of a relation ID.
     #
@@ -137,4 +178,7 @@ function name_relation_get() {
     fi
     relid="$(charms.reactive relation_id "$relation_name" "$unit_name")"
     relation-get -r "$relid" "$key" "$unit_name" "$@"
+    _restore_xtrace
 }
+
+_restore_opt $import_opts x
