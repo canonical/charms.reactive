841819e3c86ca600a2b3dadf79f85c50bdcb300a
Stuart Bishop <stuart.bishop@canonical.com>
2018-09-18 21:57:35 +0700

    Log using juju-log to avoid interleaving issues

--- charms/reactive/trace.py
+++ charms/reactive/trace.py
@@ -1,3 +1,5 @@
+from charmhelpers.core import hookenv
+
 from charms.reactive import bus
 
 
@@ -10,6 +12,11 @@ class NullTracer(object):
 
 
 class LogTracer(NullTracer):
+    LEVEL = hookenv.DEBUG
+
+    def __init__(self):
+        self._msgs = []
+
     def set_flag(self, flag):
         self._flag("flag {} set".format(flag))
 
@@ -17,22 +24,31 @@ class LogTracer(NullTracer):
         self._flag("flag {} cleared".format(flag))
 
     def _emit(self, msg):
-        print("tracer: {}".format(msg))
+        self._msgs.append("tracer: {}".format(msg))
 
-    _enabled_handlers = frozenset()
+    def _flush(self):
+        if self._msgs:
+            if len(self._msgs) > 1:
+                self._msgs.insert(0, "tracer>")
+            hookenv.log("\n".join(self._msgs), self.LEVEL)
+            self._msgs = []
+
+    _active_handlers = frozenset()
 
     def _flag(self, msg):
         self._emit(msg)
-        prev_handlers = self._enabled_handlers
+        prev_handlers = self._active_handlers
         next_handlers = set(h for h in bus.Handler.get_handlers() if h.test())
 
         for h in sorted(h.id() for h in (next_handlers - prev_handlers)):
-            self._emit("++ queue   handler {}".format(h))
+            self._emit("++   queue handler {}".format(h))
 
         for h in sorted(h.id() for h in (prev_handlers - next_handlers)):
             self._emit("-- dequeue handler {}".format(h))
 
-        self._enabled_handlers = next_handlers
+        self._flush()
+
+        self._active_handlers = next_handlers
 
 
 _tracer = None
@@ -41,6 +57,9 @@ _tracer = None
 def install_tracer(tracer):
     global _tracer
     _tracer = tracer
+    # Disable tracing when we hit atexit, to avoid spam from layers
+    # like the base layer tearing down flags.
+    hookenv.atexit(install_tracer, NullTracer())
 
 
 def tracer():
