0b88cb25fbc14d4df8cb658ff4a6d9ad50b5b85f
Cory Johns <johnsca@gmail.com>
2021-01-05 10:30:58 -0500

    Switch to GitHub Workflows for PR tests (#229)
    Travis is getting worse and worse, so this switches to GitHub Workflows
    for PR tests, and also simplifies the integration test to use the simple
    test charm already in the repo rather than the external Ghost charm.

--- tests/integration.sh
+++ tests/integration.sh
@@ -4,23 +4,9 @@ set -eux
 
 PATH=/snap/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-nonce=$(pwgen -A 8 1)
-model="reactive-$nonce"
-CHARM_DIR=$HOME/$model/charms
-BUILD_DIR=$HOME/$model/builds
+tmpdir="$(mktemp -d)"
 
-mkdir "$HOME/$model"
-mkdir "$CHARM_DIR"
-mkdir "$BUILD_DIR"
+charm build "tests/data" --build-dir "$tmpdir"
 
-charm pull-source cs:ghost "$CHARM_DIR"
-charm build "$CHARM_DIR/ghost" --build-dir "$BUILD_DIR"
-
-juju add-model "$model"
-juju switch "$model"
-wget 'https://api.jujucharms.com/charmstore/v5/ghost/resource/ghost-stable' -O ghost.zip
-juju deploy "$BUILD_DIR/ghost" --series=xenial
-juju deploy cs:haproxy
-juju attach ghost ghost-stable=./ghost.zip
-juju add-relation ghost haproxy
+juju deploy "$tmpdir/test-charm"
 juju-wait -vw
